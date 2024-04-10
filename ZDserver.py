cbbad260fd17a059bcc77e27714dca8e68f48e3f
Jinhan Zhou <53991448+lanzeweie@users.noreply.github.com>
2024-04-10 18:44:37 +0800

    移除模块，拆分功能(减轻开发者工作)
    移除无用的固定命令模块，将基础代码从分离成class，添加新功能对Post附加值的解析。

--- ZDserver.py
+++ ZDserver.py
@@ -2,12 +2,13 @@ from waitress import serve
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import datetime,socket,threading,json,sys,os,subprocess,winreg
-from Localcommand import windows_command
 import time  
 import zipfile
 # 任务栏小图标
 from tkinter import messagebox  
 from WinTaskbar import Taskbar
+# PPowerShell库
+from WinDC import PPowerShell
 
 #初始化
 #判断环境是exe还是py
@@ -21,103 +22,12 @@ app_file = os.path.basename(sys.argv[0])
 zip_file_name = time.strftime("%Y_%m_%d_%H_%M_%S") + ".zip"  
 if os.path.exists(f"{server_lujin}\log\{log_file}"):
     with zipfile.ZipFile(f"{server_lujin}\log\{zip_file_name}", 'w') as zf:
-        zf.write(f"{server_lujin}\log\{log_file}")
+        zf.write(f"{server_lujin}\log\{log_file}", arcname=log_file)
 else:
+    os.makedirs(f"{server_lujin}\log", exist_ok=True)
     with open(f"{server_lujin}\log\{log_file}", "w") as f:
         f.write("")
 log_file_name = (f"{server_lujin}\log\{log_file}")
-
-def file_json_geshihua(ipv4,port):
-    #格式化dataj json数据，设置为当机地址
-    #----------历史屎坑--------如果去除它则无法运行
-    get_ipv4_address()
-    #----------历史屎坑--------如果去除它则无法运行
-    with open(f'{server_lujin}\data\orderlist.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    for item in data:
-        if item['guding'] == 'n':
-            url_parts = item['apiUrl'].split('//')
-            if len(url_parts) > 1:
-                address = url_parts[1].split('/')[0]
-                formatted_address = address.replace(address, f'{ipv4}:{port+1}')
-                item['apiUrl'] = item['apiUrl'].replace(address, formatted_address)
-
-    with open(f'{server_lujin}\data\orderlist.json', 'w', encoding='utf-8') as file:
-        json.dump(data, file, indent=2, ensure_ascii=False)
-    print("读取了配置文件")
-
-def check_files_and_dirs():
-    files_and_dirs = [f"{server_lujin}/data"]
-    for item in files_and_dirs:  
-        if os.path.exists(item):  
-            pass  
-        else:  
-            print(f"缺失{item}")
-            messagebox.showinfo("涵涵的控制终端核心", f"缺失{item}")  
-            os._exit()
-    print("\n环境已检查：正常")
-
-
-#--------------------------------屎坑 删除则无法运行 不知道作用-----------------------
-def get_ipv4_address():  
-    ip_address = None  
-    output = subprocess.check_output('ipconfig', shell=True).decode('gbk')  
-    wireless_ip_address = get_ip_address(output, 'Wireless LAN adapter')  
-    if wireless_ip_address:  
-        return wireless_ip_address  
-    ethernet_ip_address = get_ip_address(output, 'Ethernet adapter')  
-    if ethernet_ip_address:  
-        return ethernet_ip_address  
-    any_ip_address = get_any_ip_address(output, '192.168')  
-    if any_ip_address:  
-        return any_ip_address  
-    any_ip_address = get_any_ip_address(output)  
-    return any_ip_address  
-  
-def get_ip_address(output, adapter_type):  
-    for line in output.split('\n'):  
-        line = line.strip()  
-        if line.startswith(adapter_type) or line.startswith('无线局域网适配器'):  
-            interface_name = line.split(':')[1].strip()  
-            if line.startswith('IPv4 Address') or line.startswith('IPv4 地址'):  
-                ip_address = line.split(':')[1].strip().split('(')[0]  
-                if ip_address.startswith('192.168'):  
-                    return ip_address  
-    return None  
-  
-def get_any_ip_address(output, prefix=None):  
-    for line in output.split('\n'):  
-        line = line.strip()  
-        if line.startswith('IPv4 Address') or line.startswith('IPv4 地址'):  
-            ip_address = line.split(':')[1].strip().split('(')[0]  
-            if not prefix or ip_address.startswith(prefix):  
-                return ip_address  
-    return None
-#--------------------------------屎坑 删除则无法运行 不知道作用-----------------------
-
-def get_ipv4_now():
-    ip_address = None
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 80))
-        ip_address = s.getsockname()[0]
-        s.close()
-    except socket.error:
-        pass
-    return ip_address
-
-def check_ipv4_Dynamic_state(port):
-    previous_address = None
-    while True:
-        current_address = get_ipv4_now()
-        if current_address and current_address != previous_address:
-            print("IPv4 地址发生变化：", current_address)
-            previous_address = current_address
-            #更改json地址
-            file_json_geshihua(current_address,port)
-            #更改Windows小任务栏的地址
-            Taskbar_start.icon_dongtai(current_address,port)
-        time.sleep(20)
 
 app = Flask(__name__)
 CORS(app)
@@ -180,7 +90,7 @@ class flask_api_web():
         print(nrong)
         with open(log_file_name, "a", encoding="utf-8") as f:
             f.write(nrong)
-        windows_command.shutdonw()
+        os.system("shutdown /s /f")
         response = {
             "title": "命令返回状态",
             "execution_time": current_time,
@@ -196,7 +106,7 @@ class flask_api_web():
         print(nrong)
         with open(log_file_name, "a", encoding="utf-8") as f:
             f.write(nrong)
-        windows_command.suoping()
+        os.system("rundll32.exe user32.dll,LockWorkStation")
         response = {
             "title": "命令返回状态",
             "execution_time": current_time,
@@ -205,32 +115,60 @@ class flask_api_web():
         return jsonify(response)
 
     @app.route('/command', methods=['POST'])
-    def run_commandhh():
+    def run_command_zdy():
         client_ip = request.remote_addr  # 获取客户端的 IP 地址
         current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         json_data = request.get_json()  # 获取请求中的 JSON 数据
 
         # 打印 JSON 内容和时间
-        print(f"【{current_time}】\n[控制台]: 设备 {client_ip} 发起命令hh")
+        print(f"【{current_time}】\n[控制台]: 设备 {client_ip} 发起命令 【自定义命令】")
         print("JSON 数据:", json_data)
 
-        nrong = f"【{current_time}】\n[控制台]: 设备 {client_ip} 发起命令hh\nJSON 数据: {json_data}\n"
+        nrong = f"【{current_time}】\n[控制台]: 设备 {client_ip} 发起命令 【自定义命令】\nJSON 数据: {json_data}\n"
         with open(log_file_name, "a", encoding="utf-8") as f:
             f.write(nrong)
 
         # 检查 name 字段是否为 "han han"，提取 data 字段的值
         if json_data.get("name") == "han han":
-            data_value = json_data.get("command")
-            print("data 值:", data_value)
+            data_command = json_data.get("command")
+            #检测 命令是否有用上 app文件夹中的任意程序(应该先查询app文件夹的文件)，如果有则将app文件夹中的程序作为绝对路径
+            app_files = os.listdir(f"{server_lujin}\\app")
+            print(app_files)
+            for file in app_files:
+                if file in data_command:
+                    absolute_path = os.path.join(f"{server_lujin}//app", file)
+                    data_command = data_command.replace(file, absolute_path)
+                    print("【控制台】发现命令的程序存在于[app/目录]中，将强行使用 [app/目录]中的程序作为程序源")
 
-            # 执行命令hh的逻辑
+            print("data 值:", data_command)
+            if json_data.get("value") is not None:
+                data_value = json_data.get("value")
+                print("value 值:", data_value)
+                def data_intstat(data_command,data_value):
+                    if "nircmd.exe setsysvolume" in data_command:
+                        volume = int(data_value)
+                        converted_value = int(volume * 655.35)  # 将 1-100 映射到 0-65535
+                        print(f"检测到是 nircmd.exe setsysvolume {volume}【音量调节命令】并格式化数值为 {converted_value}")
+                        data_command = data_command.replace('{value}',str(converted_value))
+                        print(data_command)
+                        return data_command
+                    else:
+                        formatted_command = data_command.replace('{value}', str(data_value))
+                        return formatted_command
+                data_command = data_intstat(data_command,data_value)
+                print(f"【控制台】最终执行命令：{data_command}")
+            
+            # 执行命令 【自定义命令】 的逻辑
             try:
-                output = subprocess.check_output(data_value, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+                output = subprocess.check_output(data_command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
                 print("命令执行结果:", output)
                 cmd_back = output
             except subprocess.CalledProcessError as e:
                 print("命令执行出错:", e.output)
                 cmd_back = e.output
+            if cmd_back == '':
+                cmd_back = "命令成功发出\n返回结果为空"
+            
         else:
             cmd_back = ""
 
@@ -275,13 +213,10 @@ if __name__ == '__main__':
     host = '0.0.0.0'
     port = 5201
     #判断是否缺失文件
-    check_files_and_dirs()
+    PPowerShell.check_files_and_dirs()
 
     #格式化文件，并实时检测ip地址是否变化，变化则根据新地址格式化文件
-    #-----------------------实时检测地址是否变化-----------------------
-    check_ipv4_Dynamic_state_duoxiancheng = threading.Thread(target=check_ipv4_Dynamic_state, args=(port,))
-    check_ipv4_Dynamic_state_duoxiancheng.daemon = True
-    check_ipv4_Dynamic_state_duoxiancheng.start()
+
     #-----------------------实时检测地址是否变化-----------------------
     
 
@@ -297,11 +232,15 @@ if __name__ == '__main__':
 
     #-----------------------Windows 小任务栏
     print("启动 Windows 小任务栏应用")
-    Taskbar_start = Taskbar(server_lujin, app_name, app_file, get_ipv4_now(), port)
+    Taskbar_start = Taskbar(server_lujin, app_name, app_file, PPowerShell.get_ipv4_now(), port)
     serve_windows_mix_icon_duoxianc = threading.Thread(target=Taskbar_start.chushihua)
     serve_windows_mix_icon_duoxianc.daemon = True
     serve_windows_mix_icon_duoxianc.start()
-    #-----------------------Windows 小任务栏
+
+    #-----------------------实时检测地址是否变化-----------------------
+    check_ipv4_Dynamic_state_duoxiancheng = threading.Thread(target=PPowerShell.check_ipv4_Dynamic_state, args=(port,Taskbar_start,))
+    check_ipv4_Dynamic_state_duoxiancheng.daemon = True
+    check_ipv4_Dynamic_state_duoxiancheng.start()
 
     #-----------------------开放检测到的端口-----------------------
     socket_thread = threading.Thread(target=Basics.run_socket, args=(host, port))
@@ -311,7 +250,7 @@ if __name__ == '__main__':
     
     #messagebox.showinfo("涵涵的控制终端核心", f"\n涵涵的控制终端核心 已成功启动 当前地址：{host}:{port}/{port+1}")  
     try:
-        print(f"\n涵涵的控制终端核心 已成功启动 当前地址：{get_ipv4_now()}:{port}/{port+1}")
+        print(f"\n涵涵的控制终端核心 已成功启动 当前地址：{PPowerShell.get_ipv4_now()}:{port}/{port+1}")
         serve(app, host=host, port=port + 1)
     except:
         print(f"Flask 服务 无法启动 端口：{port}:{port + 1} 未开放或被占用")
